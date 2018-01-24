40853579c1523e586e154b8179cb4194db7d89e2
yangyi <yhxyangyi@163.com>
2018-01-24 15:13:24 +0800

    新建脚本组件--update

--- src/scripts/new.js
+++ src/scripts/new.js
@@ -12,7 +12,7 @@ const error = console.error;
 const argv = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
 const tplDir = path.join(__dirname, '../template/');
-const pageDir = path.join(__dirname, '../webapp/features/');
+const pageDir = path.join(__dirname, '../webapp/');
 const bigCamel = (argv._[0] || '').replace(/\b(\w)(\w*)/g, ($0, $1, $2) => {
   return $1.toUpperCase() + $2;
 });
