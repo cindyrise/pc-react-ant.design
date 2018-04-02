24e26bd61dda7345edb53093c831003593e0e644
yangyi <yhxyangyi@163.com>
2018-04-02 17:37:53 +0800

    react--react-dom--update&&andt--update

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 //import createHistory from 'history/createBrowserHistory'
+//import createHistory from 'history/createBrowserHistory'
 
 import configStore from './configStore';
 import Routers from './routers';
 
 const store = configStore();
 const history =syncHistoryWithStore(hashHistory, store);
+//const history = createHistory()
 
 render(
     <Provider store={ store }>
