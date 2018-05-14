38a0fa96c32b37c699debc729a21e808307df73f
yangyi <yhxyangyi@163.com>
2018-05-14 18:02:01 +0800

    删除无关代码

--- src/webapp/pages/global/index.js
+++ src/webapp/pages/global/index.js
@@ -2,7 +2,7 @@ import global from './reducer';
 import home from '../home/reducer';
 import auth from '../auth/reducer';
 import userList from  '../user/list/reducer';
-import hello from '../hello/reducer';
+
 
 
 const appReducer = {
@@ -10,7 +10,7 @@ const appReducer = {
   home,
   auth,
   userList
- 	,hello};
+ };
 	
 	
 	
--- src/webapp/pages/hello/constant.js
+++
@@ -1,4 +0,0 @@
-import mirror from 'mirror-creator';
-  export const helloType = mirror([
-  'GET_HELLO_DATA',
-],'hello/');
